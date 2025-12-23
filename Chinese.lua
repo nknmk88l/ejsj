02977faaf065b776dfabe692c78d8fe21755b466
nknmk88l <q3776002984@QQ.com>
2025-12-23 19:51:39 +0800

    Update Chinese.lua

--- Chinese.lua
+++ Chinese.lua
@@ -233,11 +233,11 @@ local Button = Tab:Button({
 })
 
 local Button = Tab:Button({
-    Title = "隐身道具",
+    Title = "3000多个动作",
     Desc = "",
     Locked = false,
     Callback = function()
-        loadstring(game:HttpGet("https://gist.githubusercontent.com/skid123skidlol/cd0d2dce51b3f20ad1aac941da06a1a1/raw/f58b98cce7d51e53ade94e7bb460e4f24fb7e0ff/%257BFE%257D%2520Invisible%2520Tool%2520(can%2520hold%2520tools)",true))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/7yd7/Hub/refs/heads/Branch/GUIS/Emotes.lua"))()
     end
 })
 
